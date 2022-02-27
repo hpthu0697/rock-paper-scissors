82b91024153338095ef677c6544227bef702591d
Htet Pai <hpthu0697@gmail.com>
2022-02-27 12:43:31 -0500

    Reworked RPS project

--- forhtml.js
+++ forhtml.js
@@ -24,46 +24,73 @@ function playRound(playerSelection, computerSelection) {
     } 
 }
 
-function game() {
-    let playerScore = 0
-    let computerScore = 0
+function playGame() {
+    
 
-    // round 1
     
-    for (let i = 1; i < 6; i++) {
-        let playerSelection = prompt(`Round ${i} of 5: Rock, Paper, Scissors?`)
+        let playerSelection = this.textContent;
         let computerSelection = computerPlay();
-    
         playRound(playerSelection, computerSelection);
         if(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-            return alert("You must enter one of the 3 choices to play the game! Click play again!");
+            document.getElementById("messageBox").textContent = "You must enter one of the 3 choices to play the game! Click play again!";
         }
         if (playRound(playerSelection, computerSelection) === "You win!"){
-            alert(`You won!\nScore:\nYou ${playerScore + 1} - ${computerScore} Computer`);
+            document.getElementById("messageBox").textContent =
+            `You won! 
+            Score: 
+            You ${playerScore + 1} - ${computerScore} Computer`;
             playerScore++;
         } else if (playRound(playerSelection, computerSelection) === "You lose!") {
-            alert(`Computer won!\nScore:\nYou ${playerScore} - ${computerScore + 1} Computer`)
+            document.getElementById("messageBox").textContent =
+            `You lost!! 
+            Score: 
+            You ${playerScore} - ${computerScore + 1} Computer`;
             computerScore++;
         } else {
-            alert(`It's a tie!\nScore:\nYou ${playerScore} - ${computerScore} Computer`)
+            document.getElementById("messageBox").textContent =
+            `It's a tie! 
+            Score: 
+            You ${playerScore} - ${computerScore} Computer`;
+        }
+    
+        if (playerScore === 5 && playerScore > computerScore) {
+            document.getElementById("messageBox").textContent = 
+            `You won the game! 
+            The score is: 
+            You ${playerScore} - ${computerScore} Computer`;
+            playerScore = 0;
+            computerScore = 0;
+        }
+
+        if (computerScore === 5 && computerScore > playerScore) {
+            document.getElementById("messageBox").textContent = 
+            `You lost the game! 
+            The score is: 
+            You ${playerScore} - ${computerScore} Computer`;
+            playerScore = 0;
+            computerScore = 0;
         }
         
-        if (i>=5) {
-            if (playerScore > computerScore) {
-                alert("You won the game of 5!");
-            } else if (computerScore > playerScore) {
-                alert("Computer won the game of 5!");
-            } else {
-                alert("The game of 5 is a tie!"); 
-            }
-        } 
-
-       console.log(playerScore);
+        console.log(playerScore);
         console.log(computerScore);
         console.log(playerSelection);
         console.log(computerSelection);
-    }
-
+    
+    
 }
+let playerScore = 0
+let computerScore = 0
 
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", playGame)
+});
+/*
+const body = document.querySelector(".body");
+const messageBox = document.createElement("div");
+messageBox.setAttribute("id", "messageBox");
+messageBox.style.height = "50px";
+messageBox.style.border = "thick";
+messageBox.style.borderColor = "red";
 
+body.appendChild(messageBox); */
